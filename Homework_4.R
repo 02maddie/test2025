394c386fbe7177ecd6247e855ce196e27aad7885
02maddie <Maddie.Winer@uvm.edu>
2025-02-05 14:53:44 -0500

    homework 4 upload

---
+++ Homework_4.R
@@ -0,0 +1,19 @@
+# Homework 4
+# 2/5/2025
+# MEW
+
+#Question 1
+x <- 1.1
+a <- 2.2
+b <- 3.3
+
+#Part a 
+z <- x^(a^b)
+print (z)
+
+#Part b 
+z <- (x^a)^b
+print(z)
+
+#Part c
+z <- 3(x)^3+ 2(x)^2+1
